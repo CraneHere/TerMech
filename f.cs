b8e5dc81e8d7b11f3742c68102ee8958421fa2d2
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 21:58:03 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -1,24 +1,176 @@
-protected override void OnUpdateFrame(FrameEventArgs args)
+using System;
+using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace CompGraph
+{
+    public class Game : GameWindow
+    {
+        private VertexBuffer vertexBuffer;
+        private IndexBuffer indexBuffer;
+        private VertexArray vertexArray;
+        private Shader shaderProgram;
+
+        private int vertexCount;
+        private int indexCount;
+
+        private Vector2[] initialShape;
+        private Vector2[] currentShape;
+        private Matrix4 translationMatrix;
+
+        private float moveSpeed = 0.1f; // Скорость перемещения
+
+        public Game(int width = 1280, int height = 768, string title = "Game1")
+            : base(
+                  GameWindowSettings.Default,
+                  new NativeWindowSettings()
+                  {
+                      Title = title,
+                      Size = new Vector2i(width, height),
+                      WindowBorder = WindowBorder.Fixed,
+                      StartVisible = true, // Видимость окна включена для проверки
+                      StartFocused = true,
+                      API = ContextAPI.OpenGL,
+                      Profile = ContextProfile.Core,
+                      APIVersion = new Version(3, 3)
+                  })
         {
-            // Создаем матрицу трансляции, которая будет перемещать шестиугольник
-            translationMatrix = Matrix4.CreateTranslation(moveSpeed, 0, 0); // Перемещение вдоль оси X
+            this.CenterWindow();
+        }
 
-            // Применяем матрицу к каждой вершине
-            for (int i = 0; i < currentShape.Length; i++)
-            {
-                Vector4 vertex = new Vector4(currentShape[i].X, currentShape[i].Y, 0, 1);
-                Vector4 transformedVertex = translationMatrix * vertex; // Применяем матрицу трансляции
-                currentShape[i] = new Vector2(transformedVertex.X, transformedVertex.Y);
-            }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
 
-            // Обновляем буфер вершин с новыми координатами
+        protected override void OnLoad()
+        {
+            GL.ClearColor(0.8f, 0.8f, 0.8f, 1f);
+
+            this.initialShape = CreateHexagon(200, 200, 100);
+            this.currentShape = new Vector2[this.initialShape.Length];
+            Array.Copy(this.initialShape, this.currentShape, this.initialShape.Length);
+
             VertexPositionColor[] vertices = new VertexPositionColor[this.currentShape.Length];
             for (int i = 0; i < this.currentShape.Length; i++)
             {
                 vertices[i] = new VertexPositionColor(this.currentShape[i], new Color4(0.2f, 0.4f, 0.8f, 1f));
             }
 
+            this.vertexCount = vertices.Length;
+
+            int[] indices = new int[(this.vertexCount - 2) * 3];
+            for (int i = 1; i < this.vertexCount - 1; i++)
+            {
+                indices[(i - 1) * 3] = 0;
+                indices[(i - 1) * 3 + 1] = i;
+                indices[(i - 1) * 3 + 2] = i + 1;
+            }
+
+            this.indexCount = indices.Length;
+
+            this.vertexBuffer = new VertexBuffer(VertexPositionColor.VertexInfo, vertices.Length, true);
             this.vertexBuffer.SetData(vertices, vertices.Length);
+
+            this.indexBuffer = new IndexBuffer(indices.Length, true);
+            this.indexBuffer.SetData(indices, indices.Length);
+
+            this.vertexArray = new VertexArray(this.vertexBuffer);
+
+            string vertexShaderCode =
+                @"
+                #version 330 core
+
+                uniform mat4 translationMatrix;
+                uniform vec2 ViewportSize;
+                uniform float ColorFactor;
+
+                layout (location = 0) in vec2 aPosition;
+                layout (location = 1) in vec4 aColor;
+
+                out vec4 vColor;
+
+                void main()
+                {
+                    vec4 transformedPosition = translationMatrix * vec4(aPosition, 0.0, 1.0);
+                    float nx = transformedPosition.x / ViewportSize.x * 2.0 - 1.0;
+                    float ny = transformedPosition.y / ViewportSize.y * 2.0 - 1.0;
+                    gl_Position = vec4(nx, ny, 0.0, 1.0);
+                    vColor = aColor * ColorFactor;
+                }
+                ";
+
+            string pixelShaderCode =
+                @"
+                #version 330 core
+
+                in vec4 vColor;
+
+                out vec4 pixelColor;
+
+                void main()
+                {
+                    pixelColor = vColor;
+                }
+                ";
+
+            this.shaderProgram = new Shader(vertexShaderCode, pixelShaderCode);
+
+            int[] viewport = new int[4];
+            GL.GetInteger(GetPName.Viewport, viewport);
+
+            this.shaderProgram.SetUniform("ViewportSize", (float)viewport[2], (float)viewport[3]);
+
+            base.OnLoad();
+        }
+
+        private Vector2[] CreateHexagon(float centerX, float centerY, float radius)
+        {
+            Vector2[] vertices = new Vector2[6];
+            for (int i = 0; i < 6; i++)
+            {
+                float angle = MathHelper.TwoPi / 6 * i;
+                vertices[i] = new Vector2(
+                    centerX + radius * MathF.Cos(angle),
+                    centerY + radius * MathF.Sin(angle));
+            }
+            return vertices;
+        }
+
+        protected override void OnUnload()
+        {
+            this.vertexArray?.Dispose();
+            this.indexBuffer?.Dispose();
+            this.vertexBuffer?.Dispose();
+
+            base.OnUnload();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            // Обновляем матрицу трансляции для перемещения шестиугольника
+            translationMatrix = Matrix4.CreateTranslation(new Vector3(moveSpeed, 0, 0));
+            this.shaderProgram.SetUniform("translationMatrix", translationMatrix);
 
             base.OnUpdateFrame(args);
         }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(this.shaderProgram.ShaderProgramHandle);
+
+            GL.BindVertexArray(this.vertexArray.VertexArrayHandle);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, this.indexBuffer.IndexBufferHandle);
+            GL.DrawElements(PrimitiveType.Triangles, this.indexCount, DrawElementsType.UnsignedInt, 0);
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
