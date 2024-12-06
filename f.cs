424146f3e9c654b1dd12488bfb4dde606caf3aa5
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-06 21:44:48 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -1,10 +1,22 @@
-private void ApplyTransformation()
-        {
-            for (int i = 0; i < currentShape.Length; i++)
-            {
-                // Преобразуем 2D-вектор в 4D-вектор, чтобы применить матрицу 4x4
-                Vector4 vertex = new Vector4(currentShape[i], 0, 1);
-                Vector4 transformedVertex = Vector4.Transform(vertex, translationMatrix);
-                currentShape[i] = new Vector2(transformedVertex.X, transformedVertex.Y);
-            }
-        }
+protected override void OnRenderFrame(FrameEventArgs args)
+{
+    GL.Clear(ClearBufferMask.ColorBufferBit);
+
+    // Обновим данные в буфере вершин, чтобы они содержали новые координаты
+    VertexPositionColor[] vertices = new VertexPositionColor[this.currentShape.Length];
+    for (int i = 0; i < this.currentShape.Length; i++)
+    {
+        vertices[i] = new VertexPositionColor(this.currentShape[i], new Color4(0.2f, 0.4f, 0.8f, 1f));
+    }
+
+    this.vertexBuffer.SetData(vertices, vertices.Length); // Обновляем данные в буфере
+
+    GL.UseProgram(this.shaderProgram.ShaderProgramHandle);
+
+    GL.BindVertexArray(this.vertexArray.VertexArrayHandle);
+    GL.BindBuffer(BufferTarget.ElementArrayBuffer, this.indexBuffer.IndexBufferHandle);
+    GL.DrawElements(PrimitiveType.Triangles, this.indexCount, DrawElementsType.UnsignedInt, 0);
+
+    this.Context.SwapBuffers();
+    base.OnRenderFrame(args);
+}
