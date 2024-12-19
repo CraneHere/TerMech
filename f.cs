0b7812b8647078aef23585304bf1cea75f97e389
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-19 12:56:05 +0300

    Update f.cs

--- f.cs
+++ f.cs
@@ -152,4 +152,72 @@ namespace OpenTK4Scene
             }
         }
     }
+
+    public class Shader : IDisposable
+    {
+        public int Handle { get; private set; }
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            string vertexSource = System.IO.File.ReadAllText(vertexPath);
+            string fragmentSource = System.IO.File.ReadAllText(fragmentPath);
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, vertexSource);
+            GL.CompileShader(vertexShader);
+            CheckShaderCompileStatus(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragmentSource);
+            GL.CompileShader(fragmentShader);
+            CheckShaderCompileStatus(fragmentShader);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+            GL.LinkProgram(Handle);
+            CheckProgramLinkStatus(Handle);
+
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        public void SetMatrix4(string name, Matrix4 matrix)
+        {
+            int location = GL.GetUniformLocation(Handle, name);
+            GL.UniformMatrix4(location, false, ref matrix);
+        }
+
+        private static void CheckShaderCompileStatus(int shader)
+        {
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int status);
+            if (status != (int)All.True)
+            {
+                string infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Shader compilation failed: {infoLog}");
+            }
+        }
+
+        private static void CheckProgramLinkStatus(int program)
+        {
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int status);
+            if (status != (int)All.True)
+            {
+                string infoLog = GL.GetProgramInfoLog(program);
+                throw new Exception($"Program linking failed: {infoLog}");
+            }
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteProgram(Handle);
+        }
+    }
 }
