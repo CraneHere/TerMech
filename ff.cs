725e72caef7360c0bd09c8f8be9bd8391b64564b
Crane <113766015+CraneHere@users.noreply.github.com>
2024-12-19 18:23:14 +0300

    Update ff.cs

--- ff.cs
+++ ff.cs
@@ -167,3 +167,166 @@ void main()
     RayDirection = normalize(aPos);
 }
 
+-----------------------------------------------------------------------------------------------------
+
+using System;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTK4Scene
+{
+    class Program : GameWindow
+    {
+        private float _angle = 0f;
+        private float _radius = 5f;
+
+        private int _vao;
+        private int _vbo;
+        private Shader _shader;
+
+        public Program() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            Size = new Vector2i(800, 600);
+            Title = "Chill";
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            _shader = new Shader();
+            _shader.Use();
+
+            _shader.SetVector3("Sphere1Position", new Vector3(-1.5f, 0.0f, -5.0f));
+            _shader.SetVector3("Sphere2Position", new Vector3(1.5f, 0.0f, -5.0f));
+            _shader.SetVector3("Sphere3Position", new Vector3(0.0f, 1.5f, -5.0f));
+            _shader.SetVector3("PlaneNormal", new Vector3(0.0f, 1.0f, 0.0f));
+            _shader.SetFloat("SphereRadius", 1.0f);
+            _shader.SetVector3("LightPosition", new Vector3(0.0f, 5.0f, -3.0f));
+            _shader.SetVector3("CameraPosition", new Vector3(0.0f, 0.0f, 0.0f));
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            _shader.Use();
+
+            // Рисуем (рендеринг будет в шейдере)
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+
+            SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var program = new Program())
+            {
+                program.Run();
+            }
+        }
+    }
+
+    public class Shader : IDisposable
+    {
+        public int Handle { get; private set; }
+
+        public Shader(string vertexPath = "C:\\Users\\danil\\source\\repos\\CompGraph\\CompGraph\\Shaders\\shader.vert", 
+                      string fragmentPath = "C:\\Users\\danil\\source\\repos\\CompGraph\\CompGraph\\Shaders\\shader.frag")
+        {
+            string vertexSource = System.IO.File.ReadAllText(vertexPath);
+            string fragmentSource = System.IO.File.ReadAllText(fragmentPath);
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, vertexSource);
+            GL.CompileShader(vertexShader);
+            CheckShaderCompileStatus(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragmentSource);
+            GL.CompileShader(fragmentShader);
+            CheckShaderCompileStatus(fragmentShader);
+
+            Handle = GL.CreateProgram();
+            GL.AttachShader(Handle, vertexShader);
+            GL.AttachShader(Handle, fragmentShader);
+            GL.LinkProgram(Handle);
+            CheckProgramLinkStatus(Handle);
+
+            GL.DetachShader(Handle, vertexShader);
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(Handle);
+        }
+
+        public void SetMatrix4(string name, Matrix4 matrix)
+        {
+            int location = GL.GetUniformLocation(Handle, name);
+            GL.UniformMatrix4(location, false, ref matrix);
+        }
+
+        public void SetFloat(string name, float value)
+        {
+            int location = GL.GetUniformLocation(Handle, name);
+            if (location != -1)
+            {
+                GL.Uniform1(location, value);
+            }
+        }
+        public void SetVector3(string name, Vector3 vector)
+        {
+            int location = GL.GetUniformLocation(Handle, name);
+            if (location != -1)
+            {
+                GL.Uniform3(location, vector);
+            }
+        }
+
+        private static void CheckShaderCompileStatus(int shader)
+        {
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int status);
+            if (status != (int)All.True)
+            {
+                string infoLog = GL.GetShaderInfoLog(shader);
+                Console.WriteLine($"Shader Info Log: {infoLog}");
+                throw new Exception($"Shader compilation failed: {infoLog}");
+            }
+        }
+
+        private static void CheckProgramLinkStatus(int program)
+        {
+            GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int status);
+            if (status != (int)All.True)
+            {
+                string infoLog = GL.GetProgramInfoLog(program);
+                throw new Exception($"Program linking failed: {infoLog}");
+            }
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteProgram(Handle);
+        }
+    }
+}
